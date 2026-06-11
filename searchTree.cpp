b1d11149d2a1ee89cfc6321bf7955dbf5be3ccee
abiiiaja <kalindraaraya@gmail.com>
2026-06-11 18:12:18 +0700

    Membuat prosedur traverse preorder

--- searchTree.cpp
+++ searchTree.cpp
@@ -80,4 +80,20 @@ class BinaryTree{
         }
     }
 
+    void preorder(Node *ptr)
+    {
+        if (root == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            cout << ptr->info << "  ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
+
 };
