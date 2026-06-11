61cfc7861378028815d2ee34cba3a094dd2f327d
abiiiaja <kalindraaraya@gmail.com>
2026-06-11 17:50:08 +0700

    Membuat constructor berparameter

--- searchTree.cpp
+++ searchTree.cpp
@@ -8,4 +8,11 @@ class Node{
         Node *leftchild;
         Node *rightchild;
 
+        //Constructor for the node class
+        Node(string i, Node *l, Node *r){
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+
 };
